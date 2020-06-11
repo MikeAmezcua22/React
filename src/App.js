2b6a3521a8b084aa70bdf82389e4184fc2038ac0
MiguelAmezcua22 <miguelamezcua@MacBook-Air-de-Miguel.local>
2020-06-10 19:14:33 -0500

    Grid2

--- src/App.js
+++ src/App.js
@@ -16,26 +16,40 @@ function App() {
         <Grid
           lg={6}
         >
-          <div styles={styles.imageBackground} />
-        
+          <div style={styles.imageBackground} />
         </Grid>
         <Grid
         lg={6}
         >
-          Este es el inicio de mi landig page
+          <div
+            style={styles.textLandign}
+          >
+            Este es el inicio de mi landig page
+          </div>
         </Grid>
       </Grid>
     </div>
-  );
+  )
 }
 
  const styles = {
-   imageBackground:{
-    backgroundImage: "url('https://dam.cocinafacil.com.mx/wp-content/uploads/2020/04/recetas-de-comida-cuarentena.jpg')",
-    height: '500px',
-    margin: '15px',
-    backgroundSize: 'cover',
-   }
+    imageBackground: {
+      backgroundImage: "url('https://dam.cocinafacil.com.mx/wp-content/uploads/2020/04/recetas-de-comida-cuarentena.jpg')",
+      height: '500px',
+      margin: '15px',
+      backgroundSize: 'cover',
+      borderRadius: '10px',
+    },
+   textLandign:{
+     display: 'flex',
+     margin: '20px',
+     textAlign: 'center',
+     justifyContent: 'center',
+     alignItems: 'center',
+     verticalAlign: 'middle',
+     height: '500px',
+     fontSize: '30px'
+   },
  }
 
 export default App;
